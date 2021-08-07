75896b645e6bcef0ff2b33865d97ac12388dbb86
alhanouf96 <46896368+alhanouf96@users.noreply.github.com>
2021-08-07 22:17:34 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,315 @@
+
+
+
+$('#main .lazy').lazyload();
+
+
+
+  
+
+function gofor(id) {
+
+switch(id){
+
+  case "kin":
+var val = "ki"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+case "vi":
+var val = "vii"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+case "vi":
+var val = "vii"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+case "km":
+var val = "kmm"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+
+
+case "la":
+var val = "laa"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+
+
+case "sp":
+var val = "spp"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+
+case "ko":
+var val = "koo"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+
+case "ka":
+var val = "kaa"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+
+case "fl":
+var val = "fll"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+case "es":
+var val = "ess"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+
+case "am":
+var val = "amm"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+case "ym":
+var val = "ymm"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+/*tea*/
+
+
+ case "ta":
+var val = "taa"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+case "cdr":
+var val = "cdrr"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+case "cam":
+var val = "camm"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "cba":
+var val = "cbaa"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "csp":
+var val = "cspp"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "cla":
+var val = "claa"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+case "cpro":
+var val = "cproo"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+case "cpr":
+var val = "cprr"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+case "cre":
+var val = "cree"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+case "ckh":
+var val = "ckhh"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "cfr":
+var val = "cfrr"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "cbe":
+var val = "cbee"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+/*cheae*/
+
+
+ case "bg":
+var val = "bgg"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "to":
+var val = "too"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "lo":
+var val = "loo"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "no":
+var val = "noo"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "fa":
+var val = "faa"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+/*coc*/
+
+ case "ma":
+var val = "maa"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "kls":
+var val = "klss"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "cred":
+var val = "credd"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "cfo":
+var val = "cfoo"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "cdb":
+var val = "cdbb"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+/*cros*/
+
+ case "ksa":
+var val = "ksaa"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "kki":
+var val = "kkii"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "klo":
+var val = "kloo"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "kch":
+var val = "kchh"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "klt":
+var val = "kltt"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+/*bar*/
+
+ case "bza":
+var val = "bzaa"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "bma":
+var val = "bmaa"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "bfo":
+var val = "bfoo"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "bba":
+var val = "bbaa"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "bno":
+var val = "bnoo"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "bda":
+var val = "bdaa"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+ case "bre":
+var val = "bree"
+localStorage.setItem("storageName",val);
+window.location.href = "deet.html";
+ break;
+
+}
+}
+
+
+
+
+
